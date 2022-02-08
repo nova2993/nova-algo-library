0c12afd85d95161ba6c9d63fcf66d0e7812bf31e
nova2993 <nova.mishka93@gmail.com>
2022-02-08 20:25:00 +0530

    Testing module expots

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ function hello(name) {
     return "Hello " + name;
 }
 
-module.exports = hello;
+function sumOfNaturalNumbers(n) {
+    if(n <= 1) return 1;
+    return n + sumOfNaturalNumbers(n - 1);
+}
+
+module.exports = {
+    hello: hello,
+    sumOfNumbes: sumOfNaturalNumbers
+};
